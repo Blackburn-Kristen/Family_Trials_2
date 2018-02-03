2ab8371ced7a5ffe39ad8e1884768a99bad83e8f
Katrina <Katrina@Artax>
2018-02-03 15:20:53 -0700

    Added: Buying, Inventory, QuestionAndAnswer, QuestionScene, and gameDashBoard. Inventory condensed several ideas.

---
+++ Family_Trials_2/src/BYUI/CIT260/Family_Trials_2/model/QuestionScene.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BYUI.CIT260.Family_Trials_2.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Katrina
+ */
+public class QuestionScene implements Serializable{
+    //class instance variable
+    private String noToAnswer;
+    private double addInventory;
+    private Buying buying;
+    private QuestionAndAnswer questionAndAnswer;
+
+    public QuestionAndAnswer getQuestionAndAnswer() {
+        return questionAndAnswer;
+    }
+
+    public void setQuestionAndAnswer(QuestionAndAnswer questionAndAnswer) {
+        this.questionAndAnswer = questionAndAnswer;
+    }
+    
+    
+
+    public Buying getBuying() {
+        return buying;
+    }
+
+    public void setBuying(Buying buying) {
+        this.buying = buying;
+    }
+    
+    
+
+    public QuestionScene() {
+    }
+    
+    
+    
+    public String getNoToAnswer() {
+        return noToAnswer;
+    }
+
+    public void setNoToAnswer(String noToAnswer) {
+        this.noToAnswer = noToAnswer;
+    }
+
+    public double getAddInventory() {
+        return addInventory;
+    }
+
+    public void setAddInventory(double addInventory) {
+        this.addInventory = addInventory;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + Objects.hashCode(this.noToAnswer);
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.addInventory) ^ (Double.doubleToLongBits(this.addInventory) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionScene{" + "noToAnswer=" + noToAnswer + ", addInventory=" + addInventory + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final QuestionScene other = (QuestionScene) obj;
+        if (Double.doubleToLongBits(this.addInventory) != Double.doubleToLongBits(other.addInventory)) {
+            return false;
+        }
+        if (!Objects.equals(this.noToAnswer, other.noToAnswer)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
---
+++ Family_Trials_2/src/BYUI/CIT260/Family_Trials_2/model/gameDashBoard.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BYUI.CIT260.Family_Trials_2.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Katrina
+ */
+public class gameDashBoard implements Serializable {
+    
+    //Class instance variables 
+    private double statusPercents;
+    private String Inventory;
+    private Game game; 
+    private Inventory inventory;
+
+    
+    
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+    
+    
+    
+    public gameDashBoard() {
+    }
+
+    
+    
+    public double getStatusPercents() {
+        return statusPercents;
+    }
+
+    public void setStatusPercents(double statusPercents) {
+        this.statusPercents = statusPercents;
+    }
+
+    public String getInventory() {
+        return Inventory;
+    }
+
+    public void setInventory(String Inventory) {
+        this.Inventory = Inventory;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.statusPercents) ^ (Double.doubleToLongBits(this.statusPercents) >>> 32));
+        hash = 71 * hash + Objects.hashCode(this.Inventory);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final gameDashBoard other = (gameDashBoard) obj;
+        if (Double.doubleToLongBits(this.statusPercents) != Double.doubleToLongBits(other.statusPercents)) {
+            return false;
+        }
+        if (!Objects.equals(this.Inventory, other.Inventory)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "gameDashBoard{" + "statusPercents=" + statusPercents + ", Inventory=" + Inventory + '}';
+    }
+    
+    
+    
+}
