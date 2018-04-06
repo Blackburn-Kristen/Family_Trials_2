f181ac507c4eabf5252bcbcd042d6ddde57843ea
Katrina <Katrina@Artax>
2018-04-05 17:45:56 -0700

    Week 12

--- Family_Trials_2/src/BYUI/CIT260/FamilyTrial2/View/GameMenuView.java
+++ Family_Trials_2/src/BYUI/CIT260/FamilyTrial2/View/GameMenuView.java
@@ -5,17 +5,84 @@
  */
 package BYUI.CIT260.FamilyTrial2.View;
 
+import BYUI.CIT260.FamilyTrials2.exceptions.MapControlException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author krist
  */
-public class GameMenuView {
+public class GameMenuView extends View{
 
     public GameMenuView() {
+        super("Game Menu\n" 
+                + "\tM - View Map\n" 
+                + "\tI - Items in Inventory\n"
+                + "\tV - View List of Vehicles\n "
+                + "\tO - View Overall Happiness\n"
+                + "\tH - Help\n" 
+                + "\tS - Save\n"
+                + "\tE - Exit\n" );
+        
+        
     }
-    
-    public void displayGameMenuView(){
-       ErrorView.display(this.getClass().getName(),"displayGameMenuView() called.");
+
+      @Override 
+        public boolean doAction(String value){
+           
+        value = value.toUpperCase();
+        
+        switch(value.charAt(0)){
+            case 'M':
+                viewMap();
+                break;
+            case 'I':
+                viewInventory();
+                break;
+            case 'V':
+                viewListVehicles();
+                break;
+            case 'O':
+                viewStatus();
+                break;    
+            case 'H':
+                help();
+                break;
+            case 'S':
+                save();
+                break;    
+            case 'E':
+                return true;
+            default:
+                ErrorView.display(this.getClass().getName(),"Invalid menu item.");
+                return false;
+        }
+        return false;
+    }
+
+    private void viewMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void viewInventory() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void viewListVehicles() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void viewStatus() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void help() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void save() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
---
+++ Family_Trials_2/src/BYUI/CIT260/FamilyTrial2/View/SaveGameView.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BYUI.CIT260.FamilyTrial2.View;
+
+/**
+ *
+ * @author Katrina
+ */
+public class SaveGameView extends View{
+
+    public SaveGameView() {
+    }
+
+    public SaveGameView(String message){
+        this.displayMessage = message;
+    }
+
+  private String[] getInputs(){
+      
+      String[] inputs = new String[1];
+      
+      input[1] = getInputs(message)
+      
+  }
+    
+    
+
+    @Override
+    public boolean doAction(String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
